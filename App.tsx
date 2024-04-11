99af2997b49e75c7be4edae14f9c249090648ea9
Daniel Chrisostomo <daniluz1515@gmail.com>
2024-04-11 14:18:13 -0300

    Style Theme definido na aplicação

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { View, StatusBar,  } from "react-native";
+import { ThemeProvider } from "styled-components";
+import theme from "src/theme";
 import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar backgroundColor={"transparent"} barStyle={"dark-content"} translucent/>
       <Home />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components/native"
 import { SafeAreaView } from "react-native-safe-area-context";
+import theme from "src/theme";
 
 
 export const Container = styled(SafeAreaView)`
-    background-color: white;
+    background-color: ${({theme}) => theme.COLORS.WHITE};
     flex: 1;
 `
